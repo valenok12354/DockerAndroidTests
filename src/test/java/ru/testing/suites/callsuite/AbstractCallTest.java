94ede8acae0d8543a6dcc378e95c01c03ab216dc
igor <koiv.igor@gmail.com>
2020-11-16 12:25:01 +0300

    1. call record check added 2. abstract class is ok

--- src/test/java/ru/testing/suites/callsuite/AbstractCallTest.java
+++ src/test/java/ru/testing/suites/callsuite/AbstractCallTest.java
@@ -2,18 +2,34 @@ package ru.testing.suites.callsuite;
 
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.internal.matchers.NotNull;
 import org.openqa.selenium.WebElement;
 import ru.testing.AndroidTestSetUp;
 import ru.testing.Utils;
 
 import java.time.Duration;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static ru.testing.settings.SetProperties.locatorProperties;
 
 public abstract class AbstractCallTest extends AndroidTestSetUp {
     private final static String declineButtonUnLocked = "com.android.incallui:id/declinebutton";
+
+    @SneakyThrows
+    public void setUpTechnology() {
+        wait.implicitWait(driver, 10);
+        callSteps.scrollBySwipeToDirection(false, driver);
+        driver.findElementByXPath(locatorProperties().getProperty("choose_RAT")).click();
+    }
 
     @SneakyThrows
     @Disabled
@@ -23,31 +39,51 @@ public abstract class AbstractCallTest extends AndroidTestSetUp {
         utils.waitDeclineButton(driver, declineButtonUnLocked);
     }
 
-    @Test()
-    public void click() {
-        WebElement webElement = driver.findElementByXPath("//android.widget.TextView[@text='Предпочтительная сеть: только GSM']");
-        assertThat(webElement, is(true));
-        System.out.println(webElement);
+    @SneakyThrows
+    public void callToAutoDialer() {
         driver.runAppInBackground(Duration.ofSeconds(-1));
         wait.implicitWait(driver, 10);
-//        driver.findElementByXPath("//android.widget.TextView[@content-desc=\"Телефон\"]").click();
         driver.findElementByAccessibilityId("Телефон").click();
         //TODO: -change to your language AccessibilityId, or find new locator and make change
-//        driver.findElementById("com.android.contacts:id/contacts_dialpad_eight").click();
-//        driver.findElementById("com.android.contacts:id/contacts_dialpad_nine").click();
-//        driver.findElementById("com.android.contacts:id/contacts_dialpad_two").click();
-//        driver.findElementById("com.android.contacts:id/contacts_dialpad_six").click();
-//        driver.findElementById("com.android.contacts:id/contacts_dialpad_two").click();
-//        driver.findElementById("com.android.contacts:id/contacts_dialpad_zero").click();
-//        driver.findElementById("com.android.contacts:id/contacts_dialpad_zero").click();
-//        driver.findElementById("com.android.contacts:id/contacts_dialpad_zero").click();
-//        driver.findElementById("com.android.contacts:id/contacts_dialpad_zero").click();
-//        driver.findElementById("com.android.contacts:id/contacts_dialpad_nine").click();
-//        driver.findElementById("com.android.contacts:id/contacts_dialpad_nine").click();
-//        driver.findElementById("com.android.contacts:id/dialButton").click();
-//        Thread.sleep(3000);
-//        driver.findElementById("com.android.incallui:id/endButton").click();
-//        Allure.addAttachment("Any text", new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
+        driver.findElementById("com.android.contacts:id/contacts_dialpad_eight").click();
+        driver.findElementById("com.android.contacts:id/contacts_dialpad_nine").click();
+        driver.findElementById("com.android.contacts:id/contacts_dialpad_two").click();
+        driver.findElementById("com.android.contacts:id/contacts_dialpad_six").click();
+        driver.findElementById("com.android.contacts:id/contacts_dialpad_two").click();
+        driver.findElementById("com.android.contacts:id/contacts_dialpad_zero").click();
+        driver.findElementById("com.android.contacts:id/contacts_dialpad_zero").click();
+        driver.findElementById("com.android.contacts:id/contacts_dialpad_zero").click();
+        driver.findElementById("com.android.contacts:id/contacts_dialpad_zero").click();
+        driver.findElementById("com.android.contacts:id/contacts_dialpad_nine").click();
+        driver.findElementById("com.android.contacts:id/contacts_dialpad_nine").click();
+        driver.findElementById("com.android.contacts:id/dialButton").click();
+        Thread.sleep(3000);
+        driver.findElementById("com.android.incallui:id/endButton").click();
+        getCallRecord();
+//      Allure.addAttachment("Any text", new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
 
+    }
+
+    @SneakyThrows
+    public void callToInvalidNumber() {
+        driver.runAppInBackground(Duration.ofSeconds(-1));
+        wait.implicitWait(driver, 10);
+        driver.findElementByAccessibilityId("Телефон").click();
+        driver.findElementById("com.android.contacts:id/contacts_dialpad_eight").click();
+        driver.findElementById("com.android.contacts:id/contacts_dialpad_eight").click();
+        driver.findElementById("com.android.contacts:id/contacts_dialpad_eight").click();
+        driver.findElementById("com.android.contacts:id/contacts_dialpad_eight").click();
+        driver.findElementById("com.android.contacts:id/contacts_dialpad_eight").click();
+        driver.findElementById("com.android.contacts:id/contacts_dialpad_eight").click();
+        driver.findElementById("com.android.contacts:id/dialButton").click();
+        Thread.sleep(3000);
+        driver.findElementById("com.android.incallui:id/endButton").click();
+        getCallRecord();
+    }
+
+    public void getCallRecord() {
+        driver.findElementByAccessibilityId("Телефон").click();
+        List<WebElement> list = driver.findElementsById("com.android.contacts:id/primary_action_view");
+        assertTrue(list.get(0).isDisplayed());
     }
 }
--- src/test/java/ru/testing/suites/callsuite/GSMCallTest.java
+++ src/test/java/ru/testing/suites/callsuite/GSMCallTest.java
@@ -1,35 +1,14 @@
 package ru.testing.suites.callsuite;
 
-import io.appium.java_client.android.AndroidDriver;
-import io.qameta.allure.Attachment;
-import io.qameta.allure.Description;
-import io.qameta.allure.Epic;
-import io.qameta.allure.Feature;
-import io.qameta.allure.Severity;
-import io.qameta.allure.SeverityLevel;
-import io.qameta.allure.Story;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-import static ru.testing.settings.SetProperties.locatorProperties;
 
-import lombok.SneakyThrows;
-import org.junit.Before;
 import org.junit.jupiter.api.*;
-import org.junit.runner.RunWith;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.openqa.selenium.WebElement;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.testing.AndroidTestSetUp;
 import ru.testing.Application;
-import ru.testing.Utils;
-import ru.testing.settings.AndroidDriverConfigurator;
-import ru.testing.settings.AndroidDriverInitializer;
-import ru.testing.steps.CallSteps;
-
-import java.time.Duration;
 
 import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 
@@ -37,43 +16,27 @@ import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 @SpringBootTest(classes = Application.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class GSMCallTest extends AbstractCallTest {
-
-    @SneakyThrows
-    @Test()
+    @Disabled
+    @Test
     @Order(1)
+    @Override
     public void setUpTechnology() {
-        wait.implicitWait(driver, 10);
-        callSteps.scrollBySwipeToDirection(false, driver);
-        driver.findElementByXPath(locatorProperties().getProperty("choose_RAT")).click();
+        super.setUpTechnology();
         driver.findElementByXPath("//android.widget.CheckedTextView[@index='3']").click(); // 0->4/3/2G, 1->3/2G, 2->3G only 3-GSM only
-//        driver.runAppInBackground(Duration.ofSeconds(-1));
+        WebElement webElement = driver.findElementByXPath("//android.widget.TextView[@text='Предпочтительная сеть: только GSM']");
+        assertThat(webElement.getText(), is("Предпочтительная сеть: только GSM"));
     }
 
-    @Test()
-    public void click() {
-        WebElement webElement = driver.findElementByXPath("//android.widget.TextView[@text='Предпочтительная сеть: только GSM']");
-        assertThat(webElement.getText(), is("Предпочтительная сеть: только GSM" ));
-        System.out.println(webElement);
-        driver.runAppInBackground(Duration.ofSeconds(-1));
-        wait.implicitWait(driver, 10);
-//        driver.findElementByXPath("//android.widget.TextView[@content-desc=\"Телефон\"]").click();
-        driver.findElementByAccessibilityId("Телефон").click();
-        //TODO: -change to your language AccessibilityId, or find new locator and make change
-//        driver.findElementById("com.android.contacts:id/contacts_dialpad_eight").click();
-//        driver.findElementById("com.android.contacts:id/contacts_dialpad_nine").click();
-//        driver.findElementById("com.android.contacts:id/contacts_dialpad_two").click();
-//        driver.findElementById("com.android.contacts:id/contacts_dialpad_six").click();
-//        driver.findElementById("com.android.contacts:id/contacts_dialpad_two").click();
-//        driver.findElementById("com.android.contacts:id/contacts_dialpad_zero").click();
-//        driver.findElementById("com.android.contacts:id/contacts_dialpad_zero").click();
-//        driver.findElementById("com.android.contacts:id/contacts_dialpad_zero").click();
-//        driver.findElementById("com.android.contacts:id/contacts_dialpad_zero").click();
-//        driver.findElementById("com.android.contacts:id/contacts_dialpad_nine").click();
-//        driver.findElementById("com.android.contacts:id/contacts_dialpad_nine").click();
-//        driver.findElementById("com.android.contacts:id/dialButton").click();
-//        Thread.sleep(3000);
-//        driver.findElementById("com.android.incallui:id/endButton").click();
-//        Allure.addAttachment("Any text", new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
+    @Disabled
+    @Test
+    @Override
+    public void callToAutoDialer() {
+        super.callToAutoDialer();
+    }
 
+    @Test
+    @Override
+    public void callToInvalidNumber() {
+        super.callToInvalidNumber();
     }
 }
